363148debfd9b58076aeff47c716a439565878a2
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-13 18:51:12 -0300

    feat: added new batch file with options power plans

---
+++ src/script/power-plan-switcher/PowerPlanSwitcher.bat.bat
@@ -0,0 +1,71 @@
+@echo off
+setlocal
+
+set PRESS_KEY_TO_ENTER_OR_MENU="Press any 'ENTER' to return to the menu or press 'ESC' to exit."
+
+:Menu
+cls
+echo Available Power Plans:
+echo 1. Balanced
+echo 2. High Performance
+echo 3. Power Saver
+echo 4. Ultimate Performance
+
+set /p choice=Enter the number of the desired power plan:
+
+if %choice% equ 1 (
+    call :SetPowerPlan 381b4222-f694-41f0-9685-ff5bb260df2e "Balanced"
+) else if %choice% equ 2 (
+    call :SetPowerPlan 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c "High Performance"
+) else if %choice% equ 3 (
+    call :SetPowerPlan a1841308-3541-4fab-bc81-f71556f20b4a "Power Saver"
+) else if %choice% equ 4 (
+    call :SetPowerPlan e9a42b02-d5df-448d-aa00-03f14749eb61 "Ultimate Performance"
+) else (
+    echo Invalid choice. Exiting...
+    pause >nul
+    exit /b
+)
+
+echo Power plan installed successfully!
+echo.
+echo %PRESS_KEY_TO_ENTER_OR_MENU%
+choice /c 12 /n /m ""
+if %errorlevel% equ 2 (
+    exit /b
+) else (
+    goto Menu
+)
+
+:SetPowerPlan
+set "GUID=%~1"
+set "PlanName=%~2"
+
+powercfg /getactivescheme | findstr /C:"%GUID%" >nul
+if %errorlevel% equ 0 (
+    echo The "%PlanName%" power plan is already active.
+    echo.
+    echo %PRESS_KEY_TO_ENTER_OR_MENU%
+    choice /c 12 /n /m ""
+    if %errorlevel% equ 2 (
+        exit /b
+    ) else (
+        exit /b
+    )
+)
+
+powercfg /list | findstr /C:"%GUID%" >nul
+if %errorlevel% neq 0 (
+    echo.
+    echo Invalid power plan GUID. Exiting...
+    echo %PRESS_KEY_TO_ENTER_OR_MENU%
+    choice /c 12 /n /m ""
+    if %errorlevel% equ 2 (
+        exit /b
+    ) else (
+        exit /b
+    )
+)
+
+powercfg /setactive %GUID%
+exit /b
---
+++ src/script/power-plan-switcher/README.md
@@ -0,0 +1,32 @@
+# Power Plan Switcher
+
+This script allows you to easily switch between different power plans on your Windows system. It provides a menu to select the desired power plan and sets it as the active power plan and force performance.
+
+## Usage
+
+1. Run the script by executing the batch file `PowerPlanSwitcher.bat`.
+2. The available power plans will be displayed on the screen.
+3. Enter the number corresponding to the desired power plan and press Enter.
+4. The selected power plan will be set as the active power plan.
+5. The script will notify you if the power plan is already active or if an invalid choice is made.
+6. To exit the script, press Esc at any time.
+
+## Power Plans
+
+The script supports the following power plans:
+
+1. Balanced
+2. High Performance
+3. Power Saver
+4. Ultimate Performance
+
+Each power plan is associated with a specific GUID that identifies it. The script uses these GUIDs to set the active power plan.
+
+## Notes
+
+- Ensure that you run the script with administrative privileges for it to work correctly.
+- If you encounter any issues or have questions, feel free to reach out for support.
+
+---
+
+*Please note that the script provided is for informational purposes only. Use it responsibly and at your own risk.*
