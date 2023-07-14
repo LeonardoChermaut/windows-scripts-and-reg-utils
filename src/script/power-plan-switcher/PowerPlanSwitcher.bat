4f3cc7f436f9f5d95d5f581bd14f0bd3ab7931d9
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-13 22:26:55 -0300

    refactor: change file name and ordered call plans

--- src/script/power-plan-switcher/PowerPlanSwitcher.bat
+++ src/script/power-plan-switcher/PowerPlanSwitcher.bat
@@ -14,11 +14,11 @@ echo 4. Ultimate Performance
 set /p choice=Enter the number of the desired power plan:
 
 if %choice% equ 1 (
-    call :SetPowerPlan 381b4222-f694-41f0-9685-ff5bb260df2e "Balanced"
-) else if %choice% equ 2 (
-    call :SetPowerPlan 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c "High Performance"
-) else if %choice% equ 3 (
     call :SetPowerPlan a1841308-3541-4fab-bc81-f71556f20b4a "Power Saver"
+) else if %choice% equ 2 (
+    call :SetPowerPlan 381b4222-f694-41f0-9685-ff5bb260df2e "Balanced"
+) else if %choice% equ 3 (
+    call :SetPowerPlan 8c5e7fda-e8bf-4a96-9a85-a6e23a8c635c "High Performance"
 ) else if %choice% equ 4 (
     call :SetPowerPlan e9a42b02-d5df-448d-aa00-03f14749eb61 "Ultimate Performance"
 ) else (
