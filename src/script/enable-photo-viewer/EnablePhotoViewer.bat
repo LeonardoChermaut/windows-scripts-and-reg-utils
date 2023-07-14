2fcfa9614714b3a6e90fdbe2faac2aed1b253def
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-13 23:01:47 -0300

    refactor: improve code with for in variabele

--- src/script/enable-photo-viewer/EnablePhotoViewer.bat
+++ src/script/enable-photo-viewer/EnablePhotoViewer.bat
@@ -2,38 +2,25 @@
 setlocal EnableDelayedExpansion
 
 REM Check if running as administrator
-net session >nul 2>&1
-if %errorLevel% neq 0 (
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\GetAdmin.vbs"
-    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\GetAdmin.vbs"
-    "%temp%\GetAdmin.vbs"
+whoami /groups | find "S-1-5-32-544" >nul 2>&1
+if %errorlevel% neq 0 (
+    echo powershell -Command "Start-Process '%~0' -Verb RunAs"
     exit /B
 )
-del "%temp%\GetAdmin.vbs" >NUL 2>&1
+
+REM Define file extensions
+set "extensions=.bmp .dib .gif .jfif .jpe .jpeg .jpg .jxr .png"
 
 REM Enable Windows Photo Viewer in Windows 10 Registry
 echo Enabling Windows Photo Viewer...
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".bmp" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".dib" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".gif" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".jfif" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".jpe" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".jpeg" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".jpg" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".jxr" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v ".png" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
 
 REM Set Windows Photo Viewer as the default app for desired extensions
-echo Setting default app associations...
-assoc .bmp=PhotoViewer.FileAssoc.Tiff
-assoc .dib=PhotoViewer.FileAssoc.Tiff
-assoc .gif=PhotoViewer.FileAssoc.Tiff
-assoc .jfif=PhotoViewer.FileAssoc.Tiff
-assoc .jpe=PhotoViewer.FileAssoc.Tiff
-assoc .jpeg=PhotoViewer.FileAssoc.Tiff
-assoc .jpg=PhotoViewer.FileAssoc.Tiff
-assoc .jxr=PhotoViewer.FileAssoc.Tiff
-assoc .png=PhotoViewer.FileAssoc.Tiff
+for %%x in (%extensions%) do (
+    echo Setting default app association for %%x...
+    assoc %%x=PhotoViewer.FileAssoc.Tiff
+    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v "%%x" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
+)
 
 REM Display message for enabling Windows Photo Viewer
 echo Windows Photo Viewer has been enabled.
