a7ba337865438a0eca1e240657de51b78a30d3f0
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-14 21:34:24 -0300

    fix: remove all builtin

--- src/script/enable-photo-viewer/EnablePhotoViewer.bat
+++ src/script/enable-photo-viewer/EnablePhotoViewer.bat
@@ -8,22 +8,25 @@ if %errorlevel% neq 0 (
     exit /B
 )
 
-REM Define file extensions
+REM Define file variables and extensions to be associated with Windows Photo Viewer
+set PHOTO_VIEWER=PhotoViewer.FileAssoc.Tiff
+set WPV_ENABLED="Windows Photo Viewer has been enabled."
 set "extensions=.bmp .dib .gif .jfif .jpe .jpeg .jpg .jxr .png"
+set HKEY_LOCAL_MACHINE="HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations"
 
 REM Enable Windows Photo Viewer in Windows 10 Registry
 echo Enabling Windows Photo Viewer...
-reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
+reg add "%HKEY_LOCAL_MACHINE%" /f /t REG_SZ /d %PHOTO_VIEWER% >nul
 
 REM Set Windows Photo Viewer as the default app for desired extensions
 for %%x in (%extensions%) do (
     echo Setting default app association for %%x...
-    assoc %%x=PhotoViewer.FileAssoc.Tiff
-    reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows Photo Viewer\Capabilities\FileAssociations" /f /v "%%x" /t REG_SZ /d "PhotoViewer.FileAssoc.Tiff" >nul
+    assoc %%x=PHOTO_VIEWER
+    reg add "%HKEY_LOCAL_MACHINE%" /f /v "%%x" /t REG_SZ /d %PHOTO_VIEWER% >nul
 )
 
 REM Display message for enabling Windows Photo Viewer
-echo Windows Photo Viewer has been enabled.
+echo WPV_ENABLED
 
 REM Pause for message visibility
 pause
--- src/script/remove-builtin-app/RemoveBuiltInApps.ps1
+++ src/script/remove-builtin-app/RemoveBuiltInApps.ps1
@@ -1,5 +1,1 @@
-Get-AppxPackage -AllUsers | 
-where-object 
-{  
-    $_.name –notlike "*store*" -and $_.name –notlike "*calc*" -and $_.name –notlike "*windows.photos*" -and $_.name –notlike "*edge*" 
-} | Remove-AppxPackage
+Get-AppxPackage -AllUsers | Remove-AppxPackage
