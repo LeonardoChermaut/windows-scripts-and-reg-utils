4ccec03731cd0cb993e31c37ac139e16fbf7c1aa
Leonardo Chermaut <chermauts@hotmail.com>
2023-07-21 22:31:57 -0300

    refactor: improve code run w/ RunAs and remove restriction policy

--- src/script/remove-builtin-app/RemoveBuiltInApps.ps1
+++ src/script/remove-builtin-app/RemoveBuiltInApps.ps1
@@ -1,1 +1,17 @@
-Get-AppxPackage -AllUsers | Remove-AppxPackage
+# Executar como administrador
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Start-Process powershell.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs
+    exit
+}
+
+Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser -Force
+
+$appsToKeep = @(
+    "Microsoft.WindowsStore",
+    "Microsoft.WindowsCalculator",
+    "Microsoft.MicrosoftEdge"
+)
+
+Get-AppxPackage -AllUsers |
+Where-Object { $appsToKeep -notcontains $_.Name } |
+Remove-AppxPackage
