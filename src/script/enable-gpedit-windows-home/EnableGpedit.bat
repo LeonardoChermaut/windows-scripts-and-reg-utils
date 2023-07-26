25fafab3ae2c86debb9a3192d45556e89173c472
Leonardo Chermaut <101607060+LeonardoChermaut@users.noreply.github.com>
2023-07-26 14:30:43 -0300

    refactor: remove comment

--- src/script/enable-gpedit-windows-home/EnableGpedit.bat
+++ src/script/enable-gpedit-windows-home/EnableGpedit.bat
@@ -1,7 +1,6 @@
 @echo off
 pushd "%~dp0"
 
-REM Verify if the script is running as administrator
 >nul 2>&1 "%SYSTEMROOT%\System32\cacls.exe" "%SYSTEMROOT%\System32\config\system" && (
     goto :runScript
 ) || (
@@ -12,7 +11,6 @@ REM Verify if the script is running as administrator
 )
 
 :runScript
-REM Enable Gpedit for Windows 10 Home
 dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientExtensions-Package~3*.mum > nul
 dir /b %SystemRoot%\servicing\Packages\Microsoft-Windows-GroupPolicy-ClientTools-Package~3*.mum > nul
 
